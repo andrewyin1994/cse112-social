7aa1e2197636188965900e78a35918a442f73ddb
unknown <zenithparadox@gmail.com>
2017-11-21 12:42:55 -0800

    interface in main.js

---
+++ public/hw4/js/main.js
@@ -0,0 +1,46 @@
+/*
+Goal:
+Add functionality for at least 5 features:
+1) login
+2) logout
+3) scheduling
+4) roster
+5) statistics
+
+useful:
+https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+use localStorage, which is a built-in object in the browser/webAPI/ that allows you to store key value pairs,
+use it to store arrays of objects or whatever kind of data you need
+(key k: value v, v can be any kind of thing, even an array[]) 
+it is stored until the browser session ends
+*/
+
+function logIn(/*usr, pwd*/) {
+    /* TODO */
+    return false;
+}
+
+function logOut() {
+    /* TODO */
+    return false;
+}
+
+function handleSchedule() {
+    /* TODO */
+    return false;
+}
+
+function createRoster() {
+    /* TODO */
+    return false;
+}
+
+function appendRoster() {
+    /* TODO */
+    return false;
+}
+
+function handleStatistics(){
+    /* TODO */
+    return false;
+}
