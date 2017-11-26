44dec5af8a28ad6df6e79907d3816cb781d21da0
unknown <zenithparadox@gmail.com>
2017-11-26 07:08:36 -0800

    basic rendering functionality added to roster

--- public/hw4/js/main.js
+++ public/hw4/js/main.js
@@ -41,7 +41,7 @@ function test2() {
     //grab the current username and password
     usr = document.loginForm.user.value;
     pwd = document.loginForm.password.value;
-    if(test=='usr') {
+    if(usr=='usr') {
         window.location.href = "team.html";
         return true;
     }
@@ -53,15 +53,25 @@ function test2() {
    
 }
 
-function verify() {
+function verifyLogin() {
+    //grab the current username and password
+    usr = document.loginForm.user.value;
+    pwd = document.loginForm.password.value;
+    if(usr=='usr') {
+        if(pwd=='pwd'){
+            window.location.href = "team.html";
+            return true;
+        }
+        return false;
+    }
+    else {
+        console.log("wrong");
+        //alert("wrong");
+        return false;
+    }
     
 }
 
-function logIn(/*usr, pwd*/) {
-    /* TODO */
-
-    return false;
-}
 
 function logOut() {
     /* TODO */
@@ -87,3 +97,113 @@ function handleStatistics(){
     /* TODO */
     return false;
 }
+
+const TeamSnip = { 
+    currentTeam : undefined,
+
+    util : {
+         uuid : function () {
+                    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+                    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+                    );
+                  }
+    }
+
+};
+
+class Player {
+    
+            constructor (name,number,position = 'Not assigned') {
+                this.playerId = TeamSnip.util.uuid();
+                this.name = name;
+                this.number = number;
+                this.position = position;
+                this.archived = false;
+            }
+    
+            edit(name, number, position) {
+                this.name = name;
+                this.number = number;
+                this.position = position;
+            }
+    
+            remove() {
+                this.archived = true;
+            }
+    
+            restore() {
+                this.archived = false;
+            }
+    
+            get() {
+                if (!this.archived) {
+                 return {
+                    id : this.playerId,
+                    name : this.name,
+                    number : this.number,
+                    position : this.position
+                   };
+                }
+            }
+    
+            render() {
+              let MARKUP = '';
+              if (!this.archived) {
+                MARKUP = `<li class="player">${this.name} # ${this.number} <br>
+                                 <strong>${this.position}</strong>
+                                 <div class="recordControls">
+                                  [ <span class="editBtn" onclick="TeamSnip.currentTeam.roster.renderAddForm(this.id)" id="${this.playerId}">Edit</span> 
+                                  ] &nbsp;&nbsp; [
+                                  <span class="editBtn" onclick="TeamSnip.currentTeam.removePlayer(this.id); TeamSnip.currentTeam.renderRoster()" id="${this.playerId}">Delete</span> 
+                                  ]
+                                  </div>
+                                </li>`;
+                }  
+                MARKUP = `<li class="player">
+                
+                ${this.name} # ${this.number}
+                <br>
+                <strong>${this.position}</strong>
+                <div class="recordControls">
+                [ <span class="editBtn" onclick="TeamSnip.currentTeam.roster.renderAddForm(this.id)" id="${this.playerId}">Edit</span> 
+                ] &nbsp;&nbsp; [
+                <span class="editBtn" onclick="TeamSnip.currentTeam.removePlayer(this.id); TeamSnip.currentTeam.renderRoster()" id="${this.playerId}">Delete</span> 
+                ]
+                ] &nbsp;&nbsp; [
+                <a href="player.html">view info</a>
+                ]
+                </div>
+                </li>`;
+              return MARKUP;
+            } 
+               
+        } /* Player */
+roster = [];
+var player = new Player('ABC', '123', 'Goalkeeper');
+roster.push(player);
+
+
+function render() {
+    
+    let template = document.querySelector('#roster');
+
+    let markup = '<ul>';
+     for (let i=0, len = roster.length; i < len; i++) {
+       markup += roster[i].render(); 
+     }
+    markup += '</ul>';
+
+    template.content.querySelector('#playerList').innerHTML = markup;
+
+    let clonedTemplate = document.importNode(template.content, true);
+   
+    let view = document.querySelector('#view');
+    view.innerHTML = "";
+    view.appendChild(clonedTemplate); 
+
+    // document.querySelector('#addBtn').addEventListener('click', function () {
+    //    TeamSnip.currentTeam.roster.renderAddForm();
+    // }, false);
+
+   
+   }
