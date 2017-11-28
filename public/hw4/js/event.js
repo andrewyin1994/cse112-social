07f4c6b8718551a96a5ce8b4f4b069698d120f51
Andrew Yin <a5yin@ucsd.edu>
2017-11-27 21:06:34 -0800

    event in progress

--- public/hw4/js/event.js
+++ public/hw4/js/event.js
@@ -9,7 +9,24 @@ class EventFeed {
         this.events.push(event);
     }
 
+    render() {
+        console.log(events.length);
+        let template = document.querySelector('#events');
+        let markup = '<ul>';
+        for (let i=0, len = events.length; i < len; i++) {
+            markup += events[i].render(); 
+        }
+        markup += '</ul>';
 
+        template.content.querySelector('#eventList').innerHTML = markup;
+    
+        let clonedTemplate = document.importNode(template.content, true);
+        
+        let view = document.querySelector('#view');
+        view.innerHTML = "";
+        view.appendChild(clonedTemplate);
+
+    }
 }
 
 class Event {
@@ -18,9 +35,26 @@ class Event {
         this.name = name;
         this.event = event;
     }
+
+    render() {
+        let MARKUP = '';
+        if (!this.archived) {
+        MARKUP = `<li class="event btn">
+            ${this.name} # ${this.name}
+            <br>
+            <strong>${this.name}</strong>
+            </li>`;
+        }
+        return MARKUP;
+    }
+
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     //read in from localStorage
+    let events = new EventFeed();
+    console.log('hi');
+    events.addEvent('Lionel Messi','Goal');
+    events.render();
 }
 , false);
