bdf6d03585dc5989e21b06ed365cfdb1b027d501
Judd Gatbonton <juddgatbonton@MacBook-Pro-7.local>
2017-12-08 15:03:49 -0800

    Firestore test working

---
+++ public/hw5/js/testFirestore.js
@@ -0,0 +1,30 @@
+// initialize Firebase
+var config = {
+    apiKey: "AIzaSyD2KbGQQLhhapSRQCa9y3cden7rmSL28tM",
+    authDomain: "cse134gahuyi.firebaseapp.com",
+    databaseURL: "https://cse134gahuyi.firebaseio.com",
+    projectId: "cse134gahuyi",
+    storageBucket: "cse134gahuyi.appspot.com",
+    messagingSenderId: "741715887321"
+};
+firebase.initializeApp(config);
+var firestore = firebase.firestore();
+
+const docRef = firestore.doc("samples/testData");
+
+// create query selectors first for ids
+const outputHeader = document.querySelector("#testingOutput");
+const inputTextField = document.querySelector("#latestTestStatus");
+const saveButton = document.querySelector("#saveButton");
+
+saveButton.addEventListener("click", function() {
+    const textToSave = inputTextField.value;
+    console.log("Saving " + textToSave + " to Firestore");
+    docRef.set({
+        testStatus: textToSave
+    }).then(function() {
+        console.log("Save works! Success!");
+    }).catch(function(error){
+        console.log("Shit is not working: ", error);
+    });
+});
