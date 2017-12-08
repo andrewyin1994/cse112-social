e57fb7762e41a796d98020da6db24982633f3b32
unknown <zenithparadox@gmail.com>
2017-12-08 15:38:28 -0800

    idk

--- public/hw5/js/testFirestore.js
+++ public/hw5/js/testFirestore.js
@@ -16,6 +16,7 @@ const docRef = firestore.doc("samples/testData");
 const outputHeader = document.querySelector("#testingOutput");
 const inputTextField = document.querySelector("#latestTestStatus");
 const saveButton = document.querySelector("#saveButton");
+const loadButton = document.querySelector("#loadButton");
 
 saveButton.addEventListener("click", function() {
     const textToSave = inputTextField.value;
@@ -28,3 +29,25 @@ saveButton.addEventListener("click", function() {
         console.log("Shit is not working: ", error);
     });
 });
+
+loadButton.addEventListener("click", function(){
+    docRef.get().then(function (doc){
+        if(doc && doc.exists){
+        const myData = doc.data();
+        outputHeader.innerText = "Test status: " + myData.testStatus;
+        }
+    }).catch(function(error){
+        console.log("Got an error: ", error);
+    });
+});
+
+getRealTimeUpdates = function() {
+    docRef.onSnapshot(function (doc){
+        if(doc && doc.exists){
+            const myData = doc.data();
+            outputHeader.innerText = "Test status: " + myData.testStatus;
+        }
+    })
+}
+
+getRealTimeUpdates();
