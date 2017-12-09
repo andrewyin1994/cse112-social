9fc2cc53e0c9254780ccb7fbd4cbc8c4e09e3a0a
Andrew Yin <a5yin@ucsd.edu>
2017-12-08 23:59:32 -0800

    minified

--- public/hw5/js/main.js
+++ public/hw5/js/main.js
@@ -1,52 +1,7 @@
-const txtEmail = document.getElementById('user');
-const txtPassword = document.getElementById('password');
-const btnLogin = document.getElementById('btnLogin');
-const btnSignUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnLogout');
-
-function clearStorage() {
-    window.localStorage.clear();
-    return true;
-}
-
-function verifyLogin() {
-    //grab the current username and password
-    usr = document.loginForm.user.value;
-    pwd = document.loginForm.password.value;
-    if(usr=='usr') {
-        if(pwd=='pwd'){
-            window.location.href = "team.html";
-            return true;
-        }
-        return false;
-    }
-    else {
-        console.log("wrong");
-        return false;
-    }
-
-}
-
-/* old logout */
-function logOut() {
-    firebase.auth().signOut();
-    window.location.href = "home.html";
-    return true;
-}
-
-function clearStorage() {
-    window.localStorage.clear();
-    return true;
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js')
-        .then(function(registration) {
-            console.log('Registration successful, scope is:', registration.scope);
-        })
-        .catch(function(error) {
-            console.log('Service worker registration failed, error:', error);
-        });
-    }
-});
+const txtEmail=document.getElementById('user');const txtPassword=document.getElementById('password');const btnLogin=document.getElementById('btnLogin');const btnSignUp=document.getElementById('btnSignUp');const btnLogout=document.getElementById('btnLogout');function clearStorage(){window.localStorage.clear();return!0}
+function verifyLogin(){usr=document.loginForm.user.value;pwd=document.loginForm.password.value;if(usr=='usr'){if(pwd=='pwd'){window.location.href="team.html";return!0}
+return!1}
+else{console.log("wrong");return!1}}
+function logOut(){firebase.auth().signOut();window.location.href="home.html";return!0}
+function clearStorage(){window.localStorage.clear();return!0}
+window.addEventListener("DOMContentLoaded",function(){if('serviceWorker' in navigator){navigator.serviceWorker.register('./sw.js').then(function(registration){console.log('Registration successful, scope is:',registration.scope)}).catch(function(error){console.log('Service worker registration failed, error:',error)})}})
--- public/hw5/js/schedule.js
+++ public/hw5/js/schedule.js
@@ -1,178 +1,23 @@
-const TeamSnip = {
-	currentSchedule : undefined,
-	util : {
-		uuid : function () {
-			return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-				(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-			);
-		}
-	}
-
-
-};
-
-const docRef = firestore.doc("team/schedule")
-
-/**
- * Class representing the team"s schedule of players.
- */
-class Schedule {
-
-	/**
-	 * Set the schedule as an array of Players.
-	 */
-	constructor () {
-		this.schedule = [];
-	}
-
-	addSchedule(name, number, position,archived,load) {
-		let scheduleEl = new scheduleElement(name, number, position, archived);
-		this.schedule.push(scheduleEl);
-		window.localStorage["schedule"] = JSON.stringify(TeamSnip.currentSchedule.schedule);
-		console.log(load);
-		if(load) {
-			docRef.set({team: window.localStorage["schedule"]});
-		}
-	}
-
-	editSchedule(playerId, name, number, position) {
-		let scheduleElement = this.findSchedule(playerId);
-		scheduleElement.edit(name, number, position);
-		window.localStorage["schedule"] = JSON.stringify(TeamSnip.currentSchedule.schedule);
-		docRef.set({team: window.localStorage["schedule"]});
-	}
-
-	findSchedule(playerId) {
-		let len = this.schedule.length;
-		for (let i = 0; i < len; i++) {
-			if (this.schedule[i].playerId === playerId) {
-				return(this.schedule[i]);
-			}
-		}
-		return -1;
-	}
-
-	removeSchedule(playerId) {
-		let scheduleElement = this.findSchedule(playerId);
-		scheduleElement.remove();
-		window.localStorage["schedule"] = JSON.stringify(TeamSnip.currentSchedule.schedule);
-		docRef.set({team: window.localStorage["schedule"]});
-	}
-
-	get() {
-		return this.schedule;
-	}
-
-	render() {
-
-		let template = document.querySelector("#schedule");
-
-		let markup = "<ul>";
-		for (let i=0, len = schedule.length; i < len; i++) {
-			markup += schedule[i].render();
-		}
-		for (let i=0, len = TeamSnip.currentSchedule.schedule.length; i < len; i++) {
-			markup += TeamSnip.currentSchedule.schedule[i].render();
-		}
-		markup += "</ul>";
-
-		template.content.querySelector("#playerList").innerHTML = markup;
-
-		let clonedTemplate = document.importNode(template.content, true);
-
-		let view = document.querySelector("#view");
-		view.innerHTML = "";
-		view.appendChild(clonedTemplate);
-
-		document.querySelector("#addBtn").addEventListener("click", function () {
-			TeamSnip.currentSchedule.renderAddForm();
-		}, false);
-
-	}
-
-	renderAddForm(playerId) {
-		let template = document.querySelector("#playerFormTemplate"),
-			clonedTemplate = document.importNode(template.content, true),
-			view = document.querySelector("#view");
-
-
-		view.innerHTML = "";
-		view.appendChild(clonedTemplate);
-
-		if (playerId) {
-			let player = TeamSnip.currentSchedule.findSchedule(playerId);
-
-			document.querySelector("#playerName").value = player.name;
-			document.querySelector("#playerPosition").value = player.position;
-			document.querySelector("#playerNumber").value = player.number;
-			document.querySelector("#addPlayerBtn").setAttribute("data-action","edit");
-			document.querySelector("#addPlayerBtn").setAttribute("data-playerid",playerId);
-		}
-
-		document.querySelector("#addPlayerBtn").addEventListener("click", function () {
-
-			let name, position, number,playerId;
-
-			name = document.querySelector("#playerName").value;
-			position = document.querySelector("#playerPosition").value;
-			number = document.querySelector("#playerNumber").value;
-			playerId = document.querySelector("#addPlayerBtn").getAttribute("data-playerid");
-
-			if (playerId) {
-				TeamSnip.currentSchedule.editSchedule(playerId,name,number,position);
-			} else {
-				TeamSnip.currentSchedule.addSchedule(name,number,position,false,1);
-			}
-
-			TeamSnip.currentSchedule.render();
-		}, false);
-
-		document.querySelector("#cancelPlayerBtn").addEventListener("click", function () {
-			TeamSnip.currentSchedule.render();
-		}, false);
-	}
-
-}
-
-class scheduleElement {
-
-	constructor (name,number,position = "Not assigned",archived) {
-		this.playerId = TeamSnip.util.uuid();
-		this.name = name;
-		this.number = number;
-		this.position = position;
-		this.archived = archived;
-	}
-
-	edit(name, number, position) {
-		this.name = name;
-		this.number = number;
-		this.position = position;
-	}
-
-	remove() {
-		this.archived = true;
-	}
-
-	restore() {
-		this.archived = false;
-	}
-
-	get() {
-		if (!this.archived) {
-			return {
-				id : this.playerId,
-				name : this.name,
-				number : this.number,
-				position : this.position
-			};
-		}
-	}
-
-	render() {
-		let MARKUP = "";
-		if (!this.archived) {
-			MARKUP = `<li class="player btn">
+const TeamSnip={currentSchedule:undefined,util:{uuid:function(){return([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15>>c/4).toString(16))}}};const docRef=firestore.doc("team/schedule")
+class Schedule{constructor(){this.schedule=[]}
+addSchedule(name,number,position,archived,load){let scheduleEl=new scheduleElement(name,number,position,archived);this.schedule.push(scheduleEl);window.localStorage.schedule=JSON.stringify(TeamSnip.currentSchedule.schedule);console.log(load);if(load){docRef.set({team:window.localStorage.schedule})}}
+editSchedule(playerId,name,number,position){let scheduleElement=this.findSchedule(playerId);scheduleElement.edit(name,number,position);window.localStorage.schedule=JSON.stringify(TeamSnip.currentSchedule.schedule);docRef.set({team:window.localStorage.schedule})}
+findSchedule(playerId){let len=this.schedule.length;for(let i=0;i<len;i++){if(this.schedule[i].playerId===playerId){return(this.schedule[i])}}
+return-1}
+removeSchedule(playerId){let scheduleElement=this.findSchedule(playerId);scheduleElement.remove();window.localStorage.schedule=JSON.stringify(TeamSnip.currentSchedule.schedule);docRef.set({team:window.localStorage.schedule})}
+get(){return this.schedule}
+render(){let template=document.querySelector("#schedule");let markup="<ul>";for(let i=0,len=schedule.length;i<len;i++){markup+=schedule[i].render()}
+for(let i=0,len=TeamSnip.currentSchedule.schedule.length;i<len;i++){markup+=TeamSnip.currentSchedule.schedule[i].render()}
+markup+="</ul>";template.content.querySelector("#playerList").innerHTML=markup;let clonedTemplate=document.importNode(template.content,!0);let view=document.querySelector("#view");view.innerHTML="";view.appendChild(clonedTemplate);document.querySelector("#addBtn").addEventListener("click",function(){TeamSnip.currentSchedule.renderAddForm()},!1)}
+renderAddForm(playerId){let template=document.querySelector("#playerFormTemplate"),clonedTemplate=document.importNode(template.content,!0),view=document.querySelector("#view");view.innerHTML="";view.appendChild(clonedTemplate);if(playerId){let player=TeamSnip.currentSchedule.findSchedule(playerId);document.querySelector("#playerName").value=player.name;document.querySelector("#playerPosition").value=player.position;document.querySelector("#playerNumber").value=player.number;document.querySelector("#addPlayerBtn").setAttribute("data-action","edit");document.querySelector("#addPlayerBtn").setAttribute("data-playerid",playerId)}
+document.querySelector("#addPlayerBtn").addEventListener("click",function(){let name,position,number,playerId;name=document.querySelector("#playerName").value;position=document.querySelector("#playerPosition").value;number=document.querySelector("#playerNumber").value;playerId=document.querySelector("#addPlayerBtn").getAttribute("data-playerid");if(playerId){TeamSnip.currentSchedule.editSchedule(playerId,name,number,position)}else{TeamSnip.currentSchedule.addSchedule(name,number,position,!1,1)}
+TeamSnip.currentSchedule.render()},!1);document.querySelector("#cancelPlayerBtn").addEventListener("click",function(){TeamSnip.currentSchedule.render()},!1)}}
+class scheduleElement{constructor(name,number,position="Not assigned",archived){this.playerId=TeamSnip.util.uuid();this.name=name;this.number=number;this.position=position;this.archived=archived}
+edit(name,number,position){this.name=name;this.number=number;this.position=position}
+remove(){this.archived=!0}
+restore(){this.archived=!1}
+get(){if(!this.archived){return{id:this.playerId,name:this.name,number:this.number,position:this.position}}}
+render(){let MARKUP="";if(!this.archived){MARKUP=`<li class="player btn">
 				${this.name}; Time: ${this.number}
 				<br>
 				<strong>${this.position}</strong>
@@ -183,76 +28,12 @@ class scheduleElement {
 					<span class="editBtn" onclick="TeamSnip.currentSchedule.removeSchedule(this.id); TeamSnip.currentSchedule.render()" id="${this.playerId}">Delete</span>
 				]
 				</div>
-				</li>`;
-		}
-		return MARKUP;
-	}
-
-
-} /* Player */
-
-// function render() {
-
-//     let template = document.querySelector("#roster");
-
-//     let markup = "<ul>";
-//     // for (let i=0, len = roster.length; i < len; i++) {
-//     //     markup += roster[i].render();
-//     // }
-//     for (let i=0, len = TeamSnip.currentSchedule.schedule.length; i < len; i++) {
-//         markup += TeamSnip.currentSchedule.schedule[i].render();
-//     }
-//     markup += "</ul>";
-
-//     template.content.querySelector("#playerList").innerHTML = markup;
-
-//     let clonedTemplate = document.importNode(template.content, true);
-
-//     let view = document.querySelector("#view");
-//     view.innerHTML = "";
-//     view.appendChild(clonedTemplate);
-
-//     document.querySelector("#addBtn").addEventListener("click", function () {
-//        TeamSnip.currentSchedule.renderAddForm();
-//     }, false);
-// }
-
-function reload() {
-	for (let i = 0; i < TeamSnip.currentSchedule.schedule.length; i++) {
-		if(TeamSnip.currentSchedule.schedule[i].archived == true)
-			TeamSnip.currentSchedule.schedule[i].archived = false;
-	}
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-	TeamSnip.currentSchedule = new Schedule();    
-	if(!navigator.onLine) {   
-		if(window.localStorage["loadedSched"]) {
-			// console.log(window.localStorage["loaded"]);
-			var out = JSON.parse(window.localStorage["schedule"]);
-			// console.log(out);
-			for (let i = 0; i < out.length; i++) {
-				TeamSnip.currentSchedule.addSchedule(out[i].name,out[i].number,out[i].position,s[i].archived,0);
-			}
-			TeamSnip.currentSchedule.render();
-		}
-	}
-	else {
-		docRef.get().then(function(doc) {
-			if (doc && doc.exists) {
-				const myData = doc.data().team;
-				if(myData) {
-					var s = JSON.parse(myData);
-					window.localStorage["schedule"] = s;
-					for (let i = 0; i < s.length; i++) {
-						TeamSnip.currentSchedule.addSchedule(s[i].name,s[i].number,s[i].position,s[i].archived,0);
-					}
-				}
-			}
-			else {
-				console.log("NO DOC")
-			}
-			TeamSnip.currentSchedule.render();            
-		})
-	}
-}, false);
+				</li>`}
+return MARKUP}}
+function reload(){for(let i=0;i<TeamSnip.currentSchedule.schedule.length;i++){if(TeamSnip.currentSchedule.schedule[i].archived==!0)
+TeamSnip.currentSchedule.schedule[i].archived=!1}}
+window.addEventListener("DOMContentLoaded",function(){TeamSnip.currentSchedule=new Schedule();if(!navigator.onLine){if(window.localStorage.loadedSched){var out=JSON.parse(window.localStorage.schedule);for(let i=0;i<out.length;i++){TeamSnip.currentSchedule.addSchedule(out[i].name,out[i].number,out[i].position,s[i].archived,0)}
+TeamSnip.currentSchedule.render()}}
+else{docRef.get().then(function(doc){if(doc&&doc.exists){const myData=doc.data().team;if(myData){var s=JSON.parse(myData);window.localStorage.schedule=s;for(let i=0;i<s.length;i++){TeamSnip.currentSchedule.addSchedule(s[i].name,s[i].number,s[i].position,s[i].archived,0)}}}
+else{console.log("NO DOC")}
+TeamSnip.currentSchedule.render()})}},!1)
