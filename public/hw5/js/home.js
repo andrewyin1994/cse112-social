a2f998c7465f917ff9c36f0dd3a8586407742d84
Judd Gatbonton <juddgatbonton@MacBook-Pro-7.local>
2017-12-08 23:19:04 -0800

    finally login

--- public/hw5/js/home.js
+++ public/hw5/js/home.js
@@ -1,50 +1,33 @@
-// var config = {
-//     apiKey: "AIzaSyD2KbGQQLhhapSRQCa9y3cden7rmSL28tM",
-//     authDomain: "cse134gahuyi.firebaseapp.com",
-//     databaseURL: "https://cse134gahuyi.firebaseio.com",
-//     projectId: "cse134gahuyi",
-//     storageBucket: "cse134gahuyi.appspot.com",
-//     messagingSenderId: "741715887321"
-// };
-// firebase.initializeApp(config);
-// var firestore = firebase.firestore();
+// Login button
+btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
 
-// const txtEmail = document.getElementById('user');
-// const txtPassword = document.getElementById('password');
-// const btnLogin = document.getElementById('btnLogin');
-// const btnSignUp = document.getElementById('btnSignUp');
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
 
+});
 
-// // Login button
-// btnLogin.addEventListener('click', e => {
-//     const email = txtEmail.value;
-//     const pass = txtPassword.value;
-//     const auth = firebase.auth();
+// Sign Up button
+btnSignUp.addEventListener('click', e => {
+    // need for email verification
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
 
-//     const promise = auth.signInWithEmailAndPassword(email, pass);
-//     promise.catch(e => console.log(e.message));
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
 
-// });
-
-// // Sign Up button
-// btnSignUp.addEventListener('click', e => {
-//     // need for email verification
-//     const email = txtEmail.value;
-//     const pass = txtPassword.value;
-//     const auth = firebase.auth();
-
-//     const promise = auth.createUserWithEmailAndPassword(email, pass);
-//     promise.catch(e => console.log(e.message));
-// });
-
-// // Real time listener
-// firebase.auth().onAuthStateChanged(firebaseUser => {
-//     // checks if user exists
-//     if(firebaseUser) {
-//         console.log(firebaseUser);  
-//         window.location.href = "team.html";
-//     } else {
-//         console.log('not logged in');
-//     }
-// });
-// /* end of Firebase Authentication */
+// Real time listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    // checks if user exists
+    if(firebaseUser) {
+        console.log(firebaseUser);  
+        window.location.href = "team.html";
+    } else {
+        console.log('not logged in');
+    }
+});
+/* end of Firebase Authentication */
