5160f12cc772cb1d2405c78418c5f659eba98afd
Zichao Wu <ziw255@ucsd.edu>
2018-06-09 14:45:02 -0700

    could use components like react

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,70 @@
-// import PostComponent from './PostComponent.js';
+import PostComponent from '../PostComponent.js';
 
 // homepage js
-jQuery(function($) {
-  var $bodyEl = $('body'),
-      $sidedrawerEl = $('#sidedrawer');
+jQuery(function ($) {
+    var $bodyEl = $('body'),
+        $sidedrawerEl = $('#sidedrawer');
 
+    function showSidedrawer() {
+        // show overlay
+        var options = {
+            onclose: function () {
+                $sidedrawerEl
+                    .removeClass('active')
+                    .appendTo(document.body);
+            }
+        };
 
-  function showSidedrawer() {
-    // show overlay
-    var options = {
-      onclose: function() {
-        $sidedrawerEl
-          .removeClass('active')
-          .appendTo(document.body);
-      }
-    };
+        var $overlayEl = $(mui.overlay('on', options));
 
-    var $overlayEl = $(mui.overlay('on', options));
+        // show element
+        $sidedrawerEl.appendTo($overlayEl);
+        setTimeout(function () {
+            $sidedrawerEl.addClass('active');
+        }, 20);
+    }
 
-    // show element
-    $sidedrawerEl.appendTo($overlayEl);
-    setTimeout(function() {
-      $sidedrawerEl.addClass('active');
-    }, 20);
-  }
+    $('#post-container').append(PostComponent({content:'first'}))
+    $('#post-container').append(PostComponent({content:'second'}))
+    $('#post-container').append(PostComponent({content:'third'}))
 
-  function hideSidedrawer() {
-    $bodyEl.toggleClass('hide-sidedrawer');
-  }
+    function hideSidedrawer() {
+        $bodyEl.toggleClass('hide-sidedrawer');
+    }
 
+    $('.js-show-sidedrawer').on('click', showSidedrawer);
+    $('.js-hide-sidedrawer').on('click', hideSidedrawer);
 
-  $('.js-show-sidedrawer').on('click', showSidedrawer);
-  $('.js-hide-sidedrawer').on('click', hideSidedrawer);
-  
-  var $titleEls = $('strong', $sidedrawerEl);
+    var $titleEls = $('strong', $sidedrawerEl);
 
-  $titleEls.next().hide();
+    $titleEls
+        .next()
+        .hide();
 
-  $titleEls.on('click', function() {
-    $(this).next().slideToggle(200);
-  });
+    $titleEls.on('click', function () {
+        $(this)
+            .next()
+            .slideToggle(200);
+    });
 });
 
+// end of homepage js signout
 
+document
+    .getElementById('btnLogout')
+    .addEventListener('click', function () {
+        const currentUser = firebase
+            .auth()
+            .currentUser;
+        firebase
+            .auth()
+            .signOut()
+            .then(function () {
+                // Sign-out successful.
+                console.log(currentUser + "signed out");
 
-// end of homepage js
-
-//signout
-
-
-document.getElementById('btnLogout').addEventListener('click', function () {
-  const currentUser = firebase.auth().currentUser;
-  firebase.auth().signOut().then(function () {
-    // Sign-out successful.
-    console.log(currentUser + "signed out");
-
-    window.location.href = "index.html";
-  }, function (error) {
-    // An error happened.
-  });
-});
+                window.location.href = "index.html";
+            }, function (error) {
+                // An error happened.
+            });
+    });
