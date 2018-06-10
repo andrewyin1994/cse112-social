bf4d6835b6c5cf22d718f9543ba2776e0a878e49
James Rich <jcrich@ucsd.edu>
2018-06-10 11:31:16 -0700

    comitting changes before merging

--- public/js/authorize.js
+++ public/js/authorize.js
@@ -13,6 +13,7 @@ btnLogin.addEventListener('click', e => {
   const auth = firebase.auth();
 
   const promise = auth.signInWithEmailAndPassword(email, pass);
+  //database
   promise.catch(e => console.log(e.message));
 
   testInsert.innerHTML = "login works";
