440227eeccf5541d1646ca6f7a7eeeca27dc32e6
unknown <zenithparadox@gmail.com>
2018-06-10 18:54:05 -0700

    fixed posting

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,6 +1,29 @@
 const testBtn = document.getElementById('postBtn');
 const testInsert = document.getElementById('testInsert');
 
+function writePost(firebaseUser) {
+  const postText = document.getElementById("postText").value;
+  let addText = {
+    id: firebaseUser.uid,
+    text: postText,
+    tdee: 0,
+    time: new Date().getTime(),
+    email: firebaseUser.email
+  }
+  console.log(addText);
+  testInsert.innerHTML = JSON.stringify(addText);
+
+  firestore.collection('posts').add(
+    {
+      id: firebaseUser.uid,
+      text: postText,
+      tdee: 0,
+      time: new Date().getTime(),
+      email: firebaseUser.email
+    }
+  );
+};
+
 function sendPost(firebaseUser) {
   const cR = firestore.collection(`users/`)
   testBtn.addEventListener('click', function () {
@@ -14,15 +37,7 @@ function sendPost(firebaseUser) {
         console.log("Error: ", err);
       });
     console.log("done");
-    const postText = document.getElementById("postText").value;
-    let addText = {
-      id: firebaseUser.uid,
-      text: postText,
-      tdee: 0,
-      time: new Date().getTime(),
-      email: firebaseUser.email
-    }
-    testInsert.innerHTML = JSON.stringify(addText);
+    writePost(firebaseUser);
   });
 }
 
@@ -31,31 +46,9 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   // checks if user exists
   if (firebaseUser) {
     console.log(firebaseUser);
-    // sendPost(firebaseUser);
-    writePost(firebaseUser);
+    sendPost(firebaseUser);
   } else {
     console.log('not logged in');
   }
 });
 
-function writePost(firebaseUser) {
-  const postText = document.getElementById("postText").value;
-  let addText = {
-    id: firebaseUser.uid,
-    text: postText,
-    tdee: 0,
-    time: new Date().getTime(),
-    email: firebaseUser.email
-  }
-  console.log(addText);
-  
-  firestore.collection('posts').add(
-    {
-      id: firebaseUser.uid,
-      text: postText,
-      tdee: 0,
-      time: new Date().getTime(),
-      email: firebaseUser.email
-    }
-  );
-};
