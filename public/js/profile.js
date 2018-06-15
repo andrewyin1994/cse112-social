ef15cd083f1527e4d5384321fcb0debb64bb5ac7
jgatbonton <judd.gatbonton@gmail.com>
2018-06-14 17:54:38 -0700

    edit profile modal

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,3 +3,39 @@ function getUserName(){
         console.log(snapshot.data().name);
     });
 }
+
+editProfile.addEventListener('click', editProfileForm);
+
+function editProfileForm() {
+    // initialize modal element
+    var modalEl = document.createElement('div');
+    modalEl.style.width = '40em';
+    modalEl.style.height = '40em';
+    modalEl.style.margin = '10em auto';
+    modalEl.style.backgroundColor = '#fff';
+  
+    modalEl.innerHTML = `<div class='mui-container-fluid' style='padding-top: 3em;'>` + `<div class='mui-row'>` + `<div class='mui-col-md-10 mui-col-md-offset-1'>` +
+    `<form class="mui-form">
+    <legend>Edit Profile</legend><div class='mui-textfield mui-textfield--float-label'>
+    <input type='text'>
+    <label>Name</label>
+    </div>
+    <div class='mui-textfield mui-textfield--float-label'>
+    <input type='text'>
+    <label>Email</label>
+    </div>
+    <div class='mui-textfield mui-textfield--float-label'>
+    <input type='text'>
+    <label>Title</label>
+    </div>
+    <div class='mui-textfield mui-textfield--float-label'>
+    <textarea></textarea>
+    <label>Tell us about yourself</label>
+    </div>
+    <button class='mui-btn mui-btn--primary mui-btn--flat' id='btnPostCancel'>Cancel</button>
+    <button type='submit' class='mui-btn mui-btn--primary mui-btn--raised'>Submit</button>
+  </div></div></div></form>`;
+
+    // show modal
+    mui.overlay('on', modalEl);
+  }
